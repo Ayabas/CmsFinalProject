d51f58063b7143bc64cf10336b4af079e69dbf06
Aya Baeshean <aya@ayabas.com>
2014-06-26 12:25:25 -0500

    yql content

---
+++ js/yql.js
@@ -0,0 +1,118 @@
+jQuery20203280585315078497_1403798199539({
+    "query": {
+        "count": 1,
+        "created": "2014-06-26T15:54:40Z",
+        "lang": "en-us",
+        "diagnostics": {
+            "warning": "Invalid environment specified: store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+            "publiclyCallable": "true",
+            "url": {
+                "execution-start-time": "1",
+                "execution-stop-time": "220",
+                "execution-time": "219",
+                "content": "http://weather.yahooapis.com/forecastrss?u=f&w=20066805"
+            },
+            "user-time": "220",
+            "service-time": "219",
+            "build-version": "0.2.2602"
+        },
+        "results": {
+            "channel": {
+                "title": "Yahoo! Weather - Mannheim, DE",
+                "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Mannheim__DE/*http://weather.yahoo.com/forecast/GMXX1873_f.html",
+                "description": "Yahoo! Weather for Mannheim, DE",
+                "language": "en-us",
+                "lastBuildDate": "Thu, 26 Jun 2014 4:58 pm CEST",
+                "ttl": "60",
+                "location": {
+                    "city": "Mannheim",
+                    "country": "Germany",
+                    "region": "BW"
+                },
+                "units": {
+                    "distance": "mi",
+                    "pressure": "in",
+                    "speed": "mph",
+                    "temperature": "F"
+                },
+                "wind": {
+                    "chill": "71",
+                    "direction": "360",
+                    "speed": "7"
+                },
+                "atmosphere": {
+                    "humidity": "32",
+                    "pressure": "29.59",
+                    "rising": "0",
+                    "visibility": ""
+                },
+                "astronomy": {
+                    "sunrise": "5:18 am",
+                    "sunset": "9:36 pm"
+                },
+                "image": {
+                    "title": "Yahoo! Weather",
+                    "width": "142",
+                    "height": "18",
+                    "link": "http://weather.yahoo.com",
+                    "url": "http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif"
+                },
+                "item": {
+                    "title": "Conditions for Mannheim, DE at 4:58 pm CEST",
+                    "lat": "49.47",
+                    "long": "8.55",
+                    "link": "http://us.rd.yahoo.com/dailynews/rss/weather/Mannheim__DE/*http://weather.yahoo.com/forecast/GMXX1873_f.html",
+                    "pubDate": "Thu, 26 Jun 2014 4:58 pm CEST",
+                    "condition": {
+                        "code": "30",
+                        "date": "Thu, 26 Jun 2014 4:58 pm CEST",
+                        "temp": "71",
+                        "text": "Partly Cloudy"
+                    },
+                    "description": "\n<img src=\"http://l.yimg.com/a/i/us/we/52/30.gif\"/><br />\n<b>Current Conditions:</b><br />\nPartly Cloudy, 71 F<BR />\n<BR /><b>Forecast:</b><BR />\nThu - Mostly Clear. High: 73 Low: 53<br />\nFri - Partly Cloudy. High: 79 Low: 58<br />\nSat - PM Showers. High: 76 Low: 57<br />\nSun - Showers. High: 69 Low: 54<br />\nMon - Few Showers. High: 70 Low: 50<br />\n<br />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Mannheim__DE/*http://weather.yahoo.com/forecast/GMXX1873_f.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>\n",
+                    "forecast": [{
+                        "code": "33",
+                        "date": "26 Jun 2014",
+                        "day": "Thu",
+                        "high": "73",
+                        "low": "53",
+                        "text": "Mostly Clear"
+                    }, {
+                        "code": "30",
+                        "date": "27 Jun 2014",
+                        "day": "Fri",
+                        "high": "79",
+                        "low": "58",
+                        "text": "Partly Cloudy"
+                    }, {
+                        "code": "39",
+                        "date": "28 Jun 2014",
+                        "day": "Sat",
+                        "high": "76",
+                        "low": "57",
+                        "text": "PM Showers"
+                    }, {
+                        "code": "11",
+                        "date": "29 Jun 2014",
+                        "day": "Sun",
+                        "high": "69",
+                        "low": "54",
+                        "text": "Showers"
+                    }, {
+                        "code": "11",
+                        "date": "30 Jun 2014",
+                        "day": "Mon",
+                        "high": "70",
+                        "low": "50",
+                        "text": "Few Showers"
+                    }
+                    ],
+                    "guid": {
+                        "isPermaLink": "false",
+                        "content": "GMXX1873_2014_06_30_7_00_CEST"
+                    }
+                }
+            }
+        }
+    }
+});
